49033d419f3615a4ceb261d8d2fab157ac66b785
Brett Cadigan <brettcadigan@gmail.com>
2024-11-27 17:56:59 -0500

    Optimize to eliminate the second loop for 2x2 box win check; Add additional comments/explanation

--- assessment.js
+++ assessment.js
@@ -1,59 +1,28 @@
+/*
+The 'board' object must be a 0-indexed 4x4 matrix (2D-Array), with a character (or NULL) in each cell.
+The scorable values are 'X' and 'O'. 
+Any other character (lower-case, another character, a blank space, or NULL) are treated as an empty cell.
+
+board = [	
+			["O", "", "", "X"],
+        	["O", "", "X", ""],
+        	["O", "X", "", ""],
+        	["X", "O", "", ""]
+        ];
+
+I performed testing by passing different boards (like the above) to my functions, and confirming the winners.
+
+I chose to make the functions static since we do not need an instance of TicTacToe.
+The TicTacToe class will process any 'board' it is passed. (e.g. TicTacToe.checkWinner(exampleBoard); )
+*/
+
 class TicTacToe {
-
-	/*
-	The 'board' object must be a 0-indexed 4x4 matrix, with a character in each cell.
-	The scorable values are 'X' and 'O'. 
-	Any other character (lower-case, another character, a blank space, or NULL) are treated as an empty cell.
-	*/
-
 
 	// Check all win conditions for current board.
 	// Return 'X' if the X Player has won, Return 'O' if the O Player has won, or return NULL if neither player has won.
 	static checkWinner(board) {
 
-		// Diagonal Win
-		let diagonalLeftRightWinsX = true;
-		let diagonalRightLeftWinsX = true;
-
-		let diagonalLeftRightWinsO = true;
-		let diagonalRightLeftWinsO = true;
-
-
-		for (let i = 0; i < 4; i++) {
-			switch (board[i][i]) {
-				case 'X':
-					diagonalLeftRightWinsO = false;
-					break;
-				case 'O':
-					diagonalLeftRightWinsX = false;
-					break;
-				default:
-					diagonalLeftRightWinsO = false;
-					diagonalLeftRightWinsX = false;
-			}
-
-			switch (board[i][3-i]) {
-				case 'X':
-					diagonalRightLeftWinsO = false;
-					break;
-				case 'O':
-					diagonalRightLeftWinsX = false;
-					break;
-				default:
-					diagonalRightLeftWinsO = false;
-					diagonalRightLeftWinsX = false;
-			}
-		}
-
-		if (diagonalLeftRightWinsX || diagonalRightLeftWinsX) {
-			return 'X';
-		}
-
-		if (diagonalLeftRightWinsO || diagonalRightLeftWinsO) {
-			return 'O';
-		}
-
-		// Four Corners Win
+		// Four Corners win check
 		if (board[0][0] === 'X' && 
 			board[0][3] === 'X' &&
 			board[3][0] === 'X' &&
@@ -68,17 +37,57 @@ class TicTacToe {
 			return 'O';
 		}
 
-		// Vertical Win
+		// Use these trackers to check the win state. 
+		// They will be set to false if the method sees that a win possibility is eliminated.
+		
+		// Vertical Wins trackers
 		let columnWinsX = [true, true, true, true];
 		let columnWinsO = [true, true, true, true];
 
-		// Horizontal Win
+		// Horizontal Wins trackers
 		let rowWinsX = [true, true, true, true];
 		let rowWinsO = [true, true, true, true];
 
+		// Diagonal Wins trackers
+		let diagonalLeftRightWinsX = true;
+		let diagonalRightLeftWinsX = true;
+
+		let diagonalLeftRightWinsO = true;
+		let diagonalRightLeftWinsO = true;
+
+		// Loop through rows
 		for (let i = 0; i < 4; i++) {
+
+			// Diagonal win check (from top left corner down)
+			switch (board[i][i]) {
+				case 'X':
+					diagonalLeftRightWinsO = false;
+					break;
+				case 'O':
+					diagonalLeftRightWinsX = false;
+					break;
+				default:
+					diagonalLeftRightWinsO = false;
+					diagonalLeftRightWinsX = false;
+			}
+
+			// Diagonal win check (from top right corner down)
+			switch (board[i][3-i]) {
+				case 'X':
+					diagonalRightLeftWinsO = false;
+					break;
+				case 'O':
+					diagonalRightLeftWinsX = false;
+					break;
+				default:
+					diagonalRightLeftWinsO = false;
+					diagonalRightLeftWinsX = false;
+			}
+
+			// Loop through columns
 			for (let j = 0; j < 4; j++) {
 
+				// Row and Column win check
 			    switch (board[i][j]) {
 
 			        case 'X':
@@ -97,34 +106,39 @@ class TicTacToe {
 			            columnWinsX[j] = false;
 			            columnWinsO[j] = false;
 			    }
+
+			    // 2x2 Box win check
+			    if (i < 3 && j < 3) {
+			    	if (board[i][j] === 'X' && 
+						board[i+1][j] === 'X' &&
+						board[i][j+1] === 'X' &&
+						board[i+1][j+1] === 'X') {
+						return 'X';
+					}
+
+					if (board[i][j] === 'O' && 
+						board[i+1][j] === 'O' &&
+						board[i][j+1] === 'O' &&
+						board[i+1][j+1] === 'O') {
+						return 'O';
+					}
+			    }
 			}
 		}
 
+		// Diagonal wins return (if any are still true, we have a winner)
+		if (diagonalLeftRightWinsX || diagonalRightLeftWinsX) {
+			return 'X';
+		} else if (diagonalLeftRightWinsO || diagonalRightLeftWinsO) {
+			return 'O';
+		}
+
+		// Row and Column wins return (if any are still true, we have a winner)
         if (columnWinsO.includes(true) || rowWinsO.includes(true)) {
             return 'O';
-    
     	} else if (columnWinsX.includes(true) || rowWinsX.includes(true)) {
     	    return 'X';
     	}
-
-		// 2x2 Box
-		for (let i = 0; i < 3; i++) {
-			for (let j = 0; j < 3; j++) {
-				if (board[i][j] === 'X' && 
-					board[i+1][j] === 'X' &&
-					board[i][j+1] === 'X' &&
-					board[i+1][j+1] === 'X') {
-					return 'X';
-				}
-
-				if (board[i][j] === 'O' && 
-					board[i+1][j] === 'O' &&
-					board[i][j+1] === 'O' &&
-					board[i+1][j+1] === 'O') {
-					return 'O';
-				}
-			}
-		}
 
     	// No winner, return null
     	return null;
@@ -132,7 +146,9 @@ class TicTacToe {
 
 	// Returns bool TRUE if there are moves left or bool FALSE if there are no moves left to make.
 	static anyMovesLeft(board) {
+		// Navigate through rows
 		for (let i = 0; i < 4; i++) {
+			// Navigate through columns
 			for (let j = 0; j < 4; j++) {
 				if (board[i][j] != 'X' && board[i][j] != 'O') {
 					return true;
@@ -146,24 +162,14 @@ class TicTacToe {
 
 	// Check if there is a winner on the board. If not, check if there are any moves left to make. If either are true, the
 	static isGameOver(board) {
-		// let winner = checkWinner(board);
-		// if (winner === 'X' || winner === 'O') {
-		// 	return true;
-		// }
-
-		// TODO check if this is the correct way to pass a method to a switch statement
-		switch (winner = checkWinner(board)) {
-			case 'X':
-				// X wins!
-			case 'O':
-				// O wins!
-				return true;
-				break;
-			default:
-				// No winner (yet)
+		// Check if the game is over by a player's victory
+		let winner = this.checkWinner(board);
+		if (winner === 'X' || winner === 'O') {
+			return true;
 		}
 
-		if (!anyMovesLeft(board)) {
+		// Check if the game is over by filling up the board with pieces
+		if (!this.anyMovesLeft(board)) {
 			return true;
 		}
 
